35c889ad42e7cf04f5e5dc3a4297e0688da4aecc
renzetorino <148633398+renzetorino@users.noreply.github.com>
2025-07-31 20:13:42 +0800

    SECURE LOGOGIN

--- BuiswAIz/src/App.jsx
+++ BuiswAIz/src/App.jsx
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./login";
 import Inventory from "./inventory/inventory";
 import Supplier from "./supplier/supplier";
+import ForgotPassword from './forgotpassword';
+import ResetPassword from './ResetPassword';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/inventory" element={<Inventory />} />
         <Route path="/supplier" element={<Supplier />} />
+        
       </Routes>
     </BrowserRouter>
   );
