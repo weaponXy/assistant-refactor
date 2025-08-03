c5803c0b728caa069557451386daebd0197cc9eb
Carlito <carlitopelaez1@gmail.com>
2025-08-03 18:44:24 +0800

    dashboard

--- csharp-backend/obj/Debug/net9.0/csharp-backend.AssemblyInfo.cs
+++ csharp-backend/obj/Debug/net9.0/csharp-backend.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+543e468211ac2489d93cc61b5905382ab4f4257c")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a8af36003064d3066002a50ac93890584fa38a")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
