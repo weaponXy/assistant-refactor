0963075c79a45d3131d45e03ce82f55afe2acf91
renzetorino <148633398+renzetorino@users.noreply.github.com>
2025-09-08 17:32:47 +0800

    mm

--- csharp-backend/obj/Debug/net9.0/csharp-backend.AssemblyInfo.cs
+++ csharp-backend/obj/Debug/net9.0/csharp-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb20edb0d4fec2d5c7659a7f613b50c77d3af05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21389a2753d65ef6197d96ddba07b009048ce84")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
