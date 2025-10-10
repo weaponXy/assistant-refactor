dc18f05d01e97729791374fff8f47ab3859f92c3
renzetorino <148633398+renzetorino@users.noreply.github.com>
2025-10-11 00:09:20 +0800

    uhh

--- BuiswAIz/src/expenses/expenses.jsx
+++ BuiswAIz/src/expenses/expenses.jsx
@@ -856,17 +856,15 @@ function getInlineAttachmentsFromRow(row) {
             <ul>
               <li onClick={() => navigate("/Dashboard")}>Dashboard</li>
               <li onClick={() => navigate("/inventory")}>Inventory</li>
-              <li onClick={() => navigate("/supplier")}>Supplier</li>
-              <li onClick={() => navigate("/pos")}>Point of Sales</li>
               <li onClick={() => navigate("/TablePage")}>Sales</li>
               <li className="active">Expenses</li>
-              <li onClick={() => navigate("/PlannedPaymentsPage")}>Planned Payment</li>
               <li onClick={() => navigate("/assistant")}>AI Assistant</li>
             </ul>
-            <p className="nav-header">SUPPORT</p>
+            <p className="nav-header">RELATED</p>
             <ul>
-              <li>Help</li>
-              <li>Settings</li>
+              <li onClick={() => navigate("/supplier")}>Supplier</li>
+              <li onClick={() => navigate("/pos")}>Point of Sales</li>
+              <li onClick={() => navigate("/PlannedPaymentsPage")}>Planned Payment</li>
             </ul>
           </div>
         </aside>
